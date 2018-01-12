b4776a4e956c017a61ca8fa2160b74597e9333ff
legendani <legen.dani@gmail.com>
2018-01-12 03:29:39 -0500

    apiFinal Version 1

--- fuel/app/config/development/db.php
+++ fuel/app/config/development/db.php
@@ -6,7 +6,7 @@
 return array(
 	'default' => array(
 		'connection'  => array(
-			'dsn'        => 'mysql:host=localhost;dbname=MusicProject',
+			'dsn'        => 'mysql:host=localhost;dbname=MusicDB',
 			'username'   => 'alumnos',
 			'password'   => 'cev',
 		),
